8270d784b139bb9e2c0bf26605c5f9adcd9584bf
dario <matiasdario75@gmail.com>
2023-12-01 09:59:56 -0300

    feat: created the request controller structure for the entire table
    - Created struct todoController to bring together all controllers in the table.
    - Created function to return a memory reference from todoController.
    - Structure of the request controller for the entire table created.
    - Added each controller to their respective routes.

---
+++ api/controllers/todoController.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+
+	db "github.com/darionatias-dev/todo_golang/api/db/sqlc"
+)
+
+type todoController struct {
+	DbQueries *db.Queries
+}
+
+func NewTodoController(dbQueries *db.Queries) *todoController {
+	return &todoController{
+		DbQueries: dbQueries,
+	}
+}
+
+func (tc *todoController) Create(ctx *gin.Context) {
+
+}
+func (tc *todoController) GetOne(ctx *gin.Context) {
+
+}
+func (tc *todoController) GetAll(ctx *gin.Context) {
+
+}
+func (tc *todoController) Update(ctx *gin.Context) {
+
+}
+func (tc *todoController) UpdateStatus(ctx *gin.Context) {
+
+}
+func (tc *todoController) Delete(ctx *gin.Context) {
+
+}
--- api/routes/main.go
+++ api/routes/main.go
@@ -3,20 +3,23 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 
+	"github.com/darionatias-dev/todo_golang/api/controllers"
 	db "github.com/darionatias-dev/todo_golang/api/db/sqlc"
 )
 
 func AppRoutes(router *gin.Engine, dbQueries *db.Queries) *gin.RouterGroup {
+	todoController := controllers.NewTodoController(dbQueries)
+
 	v1 := router.Group("/v1")
 	{
 		todos := v1.Group("")
 		{
-			todos.POST("/todo")
-			todos.GET("/todo/:id")
-			todos.GET("/todos")
-			todos.PATCH("/todo/:id")
-			todos.PATCH("/todo-status/:id")
-			todos.DELETE("/todo/:id")
+			todos.POST("/todo", todoController.Create)
+			todos.GET("/todo/:id", todoController.GetOne)
+			todos.GET("/todos", todoController.GetAll)
+			todos.PATCH("/todo/:id", todoController.Update)
+			todos.PATCH("/todo-status/:id", todoController.UpdateStatus)
+			todos.DELETE("/todo/:id", todoController.Delete)
 		}
 	}
 
