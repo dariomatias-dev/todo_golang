51c24ee115ea209015fbb0c2252c0159ffd77d5b
dario <matiasdario75@gmail.com>
2023-12-01 09:15:50 -0300

    feat: created connection with the database and obtain queries
    - Created init function in main.go to load environment variables, connect to the database and obtain queries.

--- main.go
+++ main.go
@@ -1,3 +1,36 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	db "github.com/darionatias-dev/todo_golang/api/db/sqlc"
+)
+
+var dbQueries *db.Queries
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	dbDriver := os.Getenv("DATABASE_DRIVER")
+	dbUrl := os.Getenv("DATABASE_URL")
+
+	dbcon, err := sql.Open(dbDriver, dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbcon.Close()
+
+	if err := dbcon.Ping(); err != nil {
+		log.Fatal(dbcon)
+	}
+
+	dbQueries = db.New(dbcon)
+}
+
 func main() {}
